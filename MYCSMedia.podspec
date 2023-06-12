7301fd8fb1e9b3a3075ac1d7a7aaddcec99fa68e
马佳 <5307460+azbura@user.noreply.gitee.com>
2023-06-12 14:29:07 +0800

    添加火山参数

--- MYCSMedia.podspec
+++ MYCSMedia.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MYCSMedia'
-  s.version          = '1.9.6'
+  s.version          = '1.9.7'
   s.summary          = 'SZRM Media Module'
 
 
--- MYCSMedia/Classes/ViewControllers/SZ5GVC.h
+++ MYCSMedia/Classes/ViewControllers/SZ5GVC.h
@@ -12,6 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 @interface SZ5GVC : UIViewController
 @property(assign,nonatomic)NSInteger initialIndex;
 @property(strong,nonatomic)NSString * contentId;
+//火山cate
+@property(strong,nonatomic)NSString * category_name;
+//火山reqId
+@property(strong,nonatomic)NSString * requestId;
 @end
 
 NS_ASSUME_NONNULL_END
